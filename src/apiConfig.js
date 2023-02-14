361824dda9874105c8a28b87d7762a7d74c398b8
faithfollansbee <faithfollansbee@gmail.com>
2023-02-14 18:05:13 -0500

    production url in apiConfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,7 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://lit-shelf-55632.herokuapp.com',
+  production: 'https://movies-app-2muv.onrender.com',
+  // production: 'https://lit-shelf-55632.herokuapp.com',
   // production: 'https://mongodb+srv://admin:adminpassword@moviecluster.kozvt.mongodb.net/Movies?retryWrites=true&w=majority',
   // production: 'https://aqueous-atoll-85096.herokuapp.com',
   // development: 'http://localhost:4741'
